31da66068d62d24dc17ffc4cd9ee4cbbd6ba6816
Jaelson Jacinto de Araújo <48810013+jaelson001@users.noreply.github.com>
2020-10-21 14:15:51 -0300

    correção para varias snackbars
    Agora várias snackbars podem ser chamadas na mesma tela sem que uma sobreponha a outra

--- snackbar.js
+++ snackbar.js
@@ -30,16 +30,37 @@ var snackBar = function(texto, nivel){
 	    "class" : "snack-bar",
 	    "css" : {
 	    	display:"none", transition: 'all 0.3s', background: bg_nivel,
-		    padding: '13px', width: '25%', color: '#eee',
+		    padding: '13px', width: '100%', color: '#eee',
 		    'border-radius': '5px', 'box-shadow': '0px 2px 30px -7px #3337',
-		    'font-family': "Arial, sans-serif",
-		    'font-size': '0.8rem', 'font-weight': 'bold', position: 'absolute',
-		     bottom: '10px', right: '10px', height: "auto"
+		    'font-family': "Arial, sans-serif", 'font-size': '0.8rem',
+		    'font-weight': 'bold', position: 'relative',height: "auto",
+		    "margin": "5px"
 		}
 	});
+	var wrapper = $(".snack-wrapper");
 	snack.html(texto);
 	snack.show("fast");
-	$("body").append(snack);
-	setTimeout(function(){snack.hide("fast")},3000);
+	wrapper.append(snack);
+	setTimeout(function(){
+		snack.hide("fast");
+		setTimeout(function(){
+			snack.remove();
+		},1000);
+		
+	},3000);
+	
 };
+window.onload = function(){
+	var wrapper = $("<div/>",{
+	    "class" : "snack-wrapper",
+	    "css" : {
+	    	display:"flex", transition: 'all 0.3s', background: "transparent",
+		    padding: '5px', width: '30%', color: '#eee',
+		    'border-radius': '5px', position: 'fixed',
+		    bottom: '10px', right: '5%', height: "fit-content",
+		    "flex-flow": "column-reverse wrap"
+		}
+	});
+	$("body").append(wrapper);
+}
 window.snackBar = snackBar;
